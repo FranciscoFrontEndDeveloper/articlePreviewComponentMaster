5d4baafd4593b1ea0c34b4f39214e249c7173ab5
FRANCISCO <pachonetworking@gmail.com>
2023-06-24 15:24:59 -0500

    component main created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardmainComponent } from './components/cardmain/cardmain.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CardmainComponent,
+    title: 'Frontend Mentor | Article preview component',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardmainComponent } from './components/cardmain/cardmain.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardmainComponent
   ],
   imports: [
     BrowserModule,
