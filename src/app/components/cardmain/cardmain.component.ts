e5c23c886895a847c04869e1468130542dd98084
FRANCISCO <pachonetworking@gmail.com>
2023-06-24 20:04:24 -0500

    contend avatar and share created

--- src/app/components/cardmain/cardmain.component.ts
+++ src/app/components/cardmain/cardmain.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./cardmain.component.sass'],
 })
 export class CardmainComponent {
-  avatarToSocial: boolean = true;
+  avatarToSocial: boolean = false;
 }
