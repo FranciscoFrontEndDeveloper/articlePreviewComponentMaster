0af3a4981cbe6790cc730254d908fd5d0130ce67
FRANCISCO <pachonetworking@gmail.com>
2023-06-24 19:46:40 -0500

    text component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardmainComponent } from './components/cardmain/cardmain.component';
 import { ImagenmainComponent } from './components/imagenmain/imagenmain.component';
+import { TextComponent } from './components/text/text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardmainComponent,
-    ImagenmainComponent
+    ImagenmainComponent,
+    TextComponent
   ],
   imports: [
     BrowserModule,
